a4514b1312ac504ad171e2663a18615d88cb60db
Micheal <micheal@kmdwebdesigns.com>
2013-06-10 11:02:04 -0500

    Capitalize Artisan. Add note about finding out how more SAPIs show up in the logs.

--- src/filters.php
+++ src/filters.php
@@ -5,8 +5,9 @@ Route::filter('logviewer.logs', function()
     $logs = array();
     $logs['apache']['sapi'] = 'Apache';
     $logs['apache']['logs'] = array_reverse(glob(storage_path().'/logs/log-apache*'));
-    $logs['cli']['sapi'] = 'CLI (artisan)';
+    $logs['cli']['sapi'] = 'CLI (Artisan)';
     $logs['cli']['logs'] = array_reverse(glob(storage_path().'/logs/log-cli*'));
+    // @TODO Find out what sapi nginx, IIS, etc. show up as.
     
     foreach ($logs['apache']['logs'] as &$file)
     {
