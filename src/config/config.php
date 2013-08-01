1d125509b4450548634430b051f3f496cb97ebd9
Barry vd. Heuvel <barryvdh@gmail.com>
2013-08-01 15:06:03 +0200

    Add log_order option

--- src/config/config.php
+++ src/config/config.php
@@ -9,6 +9,7 @@ return array(
         'delete' => array(),
     ),
     'log_dirs' => array('app' => storage_path().'/logs'),
+    'log_order' => 'asc',   // Change to 'desc' for the latest entries first
     'per_page' => 10,
     'view'     => 'logviewer::viewer',
     
