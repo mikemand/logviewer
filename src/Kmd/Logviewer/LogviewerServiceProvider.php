f6e87db7d4b30a4af06e9aaedebbe4179d75fde6
Graham Campbell <graham@mineuk.com>
2013-11-01 13:53:35 +0000

    Corrected indentation and comments

--- src/Kmd/Logviewer/LogviewerServiceProvider.php
+++ src/Kmd/Logviewer/LogviewerServiceProvider.php
@@ -4,48 +4,52 @@ use Illuminate\Support\ServiceProvider;
 
 class LogviewerServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @access protected
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('kmd/logviewer');
-		
-		include __DIR__ . '/../../routes.php';
-		include __DIR__ . '/../../filters.php';
-		include __DIR__ . '/../../macros.php';
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @access public
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('kmd/logviewer');
+        
+        include __DIR__ . '/../../routes.php';
+        include __DIR__ . '/../../filters.php';
+        include __DIR__ . '/../../macros.php';
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['logviewer'] = $this->app->share(function($app)
+    /**
+     * Register the service provider.
+     *
+     * @access public
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['logviewer'] = $this->app->share(function($app)
         {
             return new Logviewer;
         });
-	}
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('logviewer');
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @access public
+     * @return array
+     */
+    public function provides()
+    {
+        return array('logviewer');
+    }
 
 }
