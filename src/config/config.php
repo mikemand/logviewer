79b3503621fcb8f7a845ace531a8852ee5c9a449
Graham Campbell <graham@mineuk.com>
2013-11-01 12:36:31 +0000

    Minor config fixes

--- src/config/config.php
+++ src/config/config.php
@@ -9,8 +9,9 @@ return array(
         'delete' => array(),
     ),
     'log_dirs' => array('app' => storage_path().'/logs'),
-    'log_order' => 'asc',   // Change to 'desc' for the latest entries first
+    'log_order' => 'asc', // Change to 'desc' for the latest entries first
     'per_page' => 10,
     'view'     => 'logviewer::viewer',
-    'pagination_view_name' => '',// Bootstrap 3.0 users: change this to pagination::slider
+    'pagination_view_name' => '', // Bootstrap 3.0 users: change this to 'pagination::slider'
+
 );
