9de17e1e319fb0d47c35173b6482243957cb4c53
Graham Campbell <graham@mineuk.com>
2013-11-01 13:54:29 +0000

    Minor tweaks and pagination fixes

--- src/config/config.php
+++ src/config/config.php
@@ -2,16 +2,16 @@
 
 return array(
     
-    'base_url' => 'logviewer',
-    'filters'  => array(
+    'base_url'   => 'logviewer',
+    'filters'    => array(
         'global' => array(),
         'view'   => array(),
         'delete' => array(),
     ),
-    'log_dirs' => array('app' => storage_path().'/logs'),
-    'log_order' => 'asc', // Change to 'desc' for the latest entries first
-    'per_page' => 10,
-    'view'     => 'logviewer::viewer',
-    'pagination_view_name' => '', // Bootstrap 3.0 users: change this to 'pagination::slider'
+    'log_dirs'   => array('app' => storage_path().'/logs'),
+    'log_order'  => 'asc', // Change to 'desc' for the latest entries first
+    'per_page'   => 10,
+    'view'       => 'logviewer::viewer',
+    'p_view'     => 'pagination::slider',
 
 );
